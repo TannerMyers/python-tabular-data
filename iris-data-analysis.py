415e0a19a7a2dcdab51b0cc4ae9e9d473648e9a4
Tanner C. Myers <tannercmyers@gmail.com>
2021-03-22 16:05:46 -0500

    Adding docstrings to get_regression_plot function

--- iris-data-analysis.py
+++ iris-data-analysis.py
@@ -6,6 +6,33 @@ from scipy import stats
 
 iris_df = pd.read_csv("iris.csv")
 
+setosa = iris_df[iris_df.species == "Iris_setosa"]
+versicolor = iris_df[iris_df.species == "Iris_versicolor"]
+virginica = iris_df[iris_df.species == "Iris_virginica"]
+
+def get_regression_plot(species)
+    """
+    Parameters
+    -------------------------------
+    species: pandas.core.frame.DataFrame
+        A dataframe containing morphological measurements for a given species.
+    Returns
+    -------------------------------
+    A scatterplot .png file showing the data with the regression of the x & y 
+    variables plotted onto them.
+    """
+    x = species.petal_length_cm
+    y = species.sepal_length_cm
+    regression = stats.linregress(x, y)
+    slope = regression.slope
+    intercept = regression.intercept   
+    plt.scatter(x, y, label = 'Data')
+    plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.legend()
+    plt.savefig("petal_v_sepal_length_regress.png")
+
 
 if __name__ == '__main__':
     main()
